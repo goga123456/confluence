544dfe36269218c2068edfc9a81ca64692e9c2e8
goga123456 <110159417+goga123456@users.noreply.github.com>
2023-12-15 16:02:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from aiogram import types, Bot, Dispatcher
-from aiogram.utils import executor
+
 
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
